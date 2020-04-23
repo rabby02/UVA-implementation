b4c47710b9f121649507708a9258eee3c4a5ac7d
Yasin Rabby <56226067+rabby02@users.noreply.github.com>
2020-04-23 17:47:40 +0600

    Add files via upload

---
+++ Behold my quadrangle.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int T;
+    int a,b,c,d;
+    cin>>T;
+    while(T--)
+    {
+        cin>>a>>b>>c>>d;
+        if(a==b&&b==c&&c==d&&d==a)
+        {
+            cout<<"square"<<endl;
+        }
+        else if(a==c&&b==d||a==b&&c==d||a==d&&b==c)
+        {
+            cout<<"rectangle"<<endl;
+        }
+        else if(a+b+c>=d||b+c+d>=a||c+d+a>=b||a+b+d>=c)
+        {
+            cout<<"quadrangle"<<endl;
+        }
+        else
+            cout<<"banana"<<endl;
+
+
+
+
+    }
+    return 0;
+}
